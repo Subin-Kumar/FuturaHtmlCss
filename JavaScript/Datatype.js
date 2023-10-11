41b77e7f743e3a4d9ec185311446ec9242c40f8a
Subin-Kumar <137891964+Subin-Kumar@users.noreply.github.com>
2023-10-11 21:33:20 +0530

    11/10

--- JavaScript/Datatype.js
+++
@@ -1,8 +0,0 @@
-var num=1
-var name="subi"
-var b=true
-var c
-document.write(typeof num+"<br>")
-document.write(typeof name+"<br>")
-document.write(typeof b+"<br>")
-document.write(typeof c+"<br>")
--- JavaScript/sample.js
+++
@@ -1,1 +0,0 @@
-document.write("js test")
